8a98820936c9e090cef73cb2f971de67981afedb
SrPabvliss <pablomartinvillacres@gmail.com>
2023-11-09 00:05:55 -0500

    Sign in visual modifications

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
+import { auth } from "@clerk/nextjs";
 import SignIn from "./signin";
+import { redirect } from 'next/navigation';
 
 export default function Page() {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect('/dashboard');
+  }
+
   return <SignIn />;
 };
--- app/(auth)/(routes)/sign-in/[[...sign-in]]/signin.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/signin.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { auth, useSignIn } from "@clerk/nextjs";
-import Image from "next/image";
 import { useState } from "react";
 import { redirect } from 'next/navigation';
 
@@ -9,12 +8,6 @@ export default function SignIn() {
     const [password, setPassword] = useState("");
 
     const { isLoaded, signIn, setActive } = useSignIn();
-
-    const { userId } = auth();
-
-    if (userId) {
-        redirect('/dashboard');
-    }
 
     if (!isLoaded) {
         return null;
@@ -45,63 +38,36 @@ export default function SignIn() {
     }
 
     return (
-        <>
-            {/*<form onSubmit={submit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <button>Sign in</button>
-                </div>  
-            </form>
-            */}
-            <div className="bg-gray-100 flex justify-center items-center h-screen">
-                <div className="w-1/2 h-screen hidden lg:block">
-                    <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image" className="object-cover w-full h-full" />
-                </div>
-                <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-                    <h1 className="text-2xl font-semibold text-black mb-4">Login</h1>
-                    <form onSubmit={submit}>
-                        <div className="mb-4">
-                            <label htmlFor="email" className="block text-gray-600">Username</label>
-                            <input type="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                id="username"
-                                name="username"
-                                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label htmlFor="password" className="block text-gray-600">Password</label>
-                            <input type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                id="password"
-                                name="password"
-                                className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
-                        </div>
-                        <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
-                    </form>
-                    <div className="mt-6 text-blue-500 text-center">
-                        <a href="#" className="hover:underline">Sign up Here</a>
-                    </div>
-                </div>
+        <div className="bg-gray-100 flex justify-center items-center h-screen w-full">
+            <div className="w-1/2 h-screen hidden lg:block">
+                <img src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat" alt="Placeholder Image" className="object-cover w-full h-full" />
             </div>
+            <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+                <h1 className="text-2xl font-semibold text-black mb-4">Login</h1>
+                <form onSubmit={submit}>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-gray-600">Username</label>
+                        <input type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            id="username"
+                            name="username"
+                            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-600">Password</label>
+                        <input type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            id="password"
+                            name="password"
+                            className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" />
+                    </div>
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
+                </form>
 
-        </>
+            </div>
+        </div>
     );
 }
